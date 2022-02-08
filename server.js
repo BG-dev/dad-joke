c5808c4740bf92096dc963928bf52d7c5146012f
bg-dev <nik.azizoff2016@gmail.com>
2022-02-08 15:27:16 +0300

    add leaderboard command

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const https = require('https')
 const fs = require('fs');
 
+const FILE_NAME = 'jokes'
+
 function searchJokesByTerm(term, callback){
     const options = {
         hostname: 'icanhazdadjoke.com',
@@ -28,21 +30,57 @@ function searchJokesByTerm(term, callback){
     req.end()   
   }
 
-function checkJokes(jokes){
+function checkJokesData(jokes){
     let msg = 'No jokes were found for that search term';
     if(jokes.length !== 0){
         const randomJoke = getRandomElement(jokes).joke
         msg = `Random joke: ${randomJoke}`
     }
     console.log(msg) // output random joke
-    saveDataToJsonFile('jokes', jokes)
+    getDataFromJsonFile(FILE_NAME, uploadDataInJsonFile.bind(null, jokes, FILE_NAME))
 }
 
-function saveDataToJsonFile(fileName, data){
+function saveDataToJsonFile(data, fileName){
     const file = fs.createWriteStream(`./${fileName}.json`)
     file.write(JSON.stringify(data, null, 4))
     file.end()
 }
+
+function getDataFromJsonFile(fileName, callback){
+    const file = fs.createReadStream(`./${fileName}.json`);
+    let data = ''
+    file.on('data', d => {
+        data += d;
+    })
+
+    file.on('close', () => {
+        data = JSON.parse(data)
+        callback(data);
+    })
+}
+
+function uploadDataInJsonFile(newData, fileName, data){
+    let updatedData = newData
+    if(data != false){
+        updatedData = [...data, ...newData]
+    }
+    saveDataToJsonFile(updatedData, fileName)
+}
+
+function findMostPopularElement(data){
+    let map = new Map();
+    data.forEach(elem => {
+        if(map.has(elem.id)){
+            map.set(elem.id, map.get(elem.id) + 1)
+        } else{
+            map.set(elem.id, 1)
+        }
+    })
+    
+    const mostPopularElement = Array.from(map).sort((a,b) => b[1] - a[1])[0];
+    console.log(data.find(elem => elem.id === mostPopularElement[0]).joke);
+}
+
 
 const getRandomElement = array => array[Math.floor(Math.random() * array.length)];
 
@@ -52,25 +90,26 @@ function getArgs () {
         .slice(2, process.argv.length)
         .forEach( arg => {
             if (arg.slice(0,2) === '--') {
-                args["longArgFlag"] = arg.slice(2, arg.length);
+                args.longArgFlag = arg.slice(2, arg.length);
             }
             else {
-                args["term"] = arg
+                args.term = arg
             }
     });
     return args;
 }
 
-function start(){
+function main(){
     const args = getArgs();
+    const longArgFlag = args.longArgFlag
 
-    if(args["longArgFlag"] === 'searchTerm'){
-        searchJokesByTerm(args["term"], checkJokes)
-    } else if(args["longArgFlag"] == 'leaderboard'){
-        console.log("leaderboard command")
+    if(longArgFlag === 'searchTerm'){
+        searchJokesByTerm(args.term, checkJokesData)
+    } else if(longArgFlag === 'leaderboard'){
+        getDataFromJsonFile(FILE_NAME, findMostPopularElement);
     } else {
         console.log("Unknown command");
     }
 }
 
-start()
+main()

06db91f1a2c28e0340f12c2f68e7fc07897eaa51
BG-dev <nik.azizoff2016@gmail.com>
2022-03-04 18:33:44 +0300

    code refactoring

--- server.js
+++ server.js
@@ -14,41 +14,35 @@ function executeCommand(command){
         leaderboard: executeLeaderboardCommand,
     }
 
-    return commands[command]?.()
+    commands[command]?.()
 }
 
 async function executeSearchTermCommand(){
-    try {
-        term = getArgs().term
-        if(!term) throw new Error("Enter a term for searching")
-    
-        const randomJoke = await getRandomJokeByTerm(term)
-        printRandomJoke(randomJoke.joke)
-        appendJokeToFile(randomJoke, FILE_NAME, FILE_EXTENSION)
-    } catch (error) {
-        console.log(error.message)
-    }
+    term = getArgs().term
+    if(!term) 
+        throw new Error("Enter a term for searching")
+
+    const randomJoke = await getRandomJokeByTerm(term)
+    printRandomJoke(randomJoke.joke)
+    appendJokeToFile(randomJoke, FILE_NAME, FILE_EXTENSION)
 }
 
 async function executeLeaderboardCommand(){
-    try {
-        const jokes = await getJokesFromFile(FILE_NAME, FILE_EXTENSION)
-        const mostPopularJoke = getMostPopularJoke(jokes)
+    const jokes = await getJokesFromFile(FILE_NAME, FILE_EXTENSION)
+    const mostPopularJoke = getMostPopularJoke(jokes)
 
-        printLeaderboard(mostPopularJoke)
-
-    } catch (error) {
-        console.log(error.message)
-    }
+    printLeaderboard(mostPopularJoke)
 }
 
 const getRandomNumberByRange = range => Math.floor(Math.random() * range) + 1
 
 async function getRandomJokeByTerm(term){
-    if(!term) throw new Error('Incorrect term!')
+    if(!term) 
+        throw new Error('Incorrect term!')
 
     const jokesMetadata = await getJokesMetadataByTerm(term)
-    if(jokesMetadata.results.length === 0) throw new Error('Joke was not found')
+    if(jokesMetadata.results.length === 0) 
+        throw new Error('Joke was not found')
 
     const randomPage = getRandomPage(jokesMetadata.total_pages)
     const randomNumberFromPage = getRandomNumberFromPage(jokesMetadata)
@@ -59,7 +53,8 @@ async function getRandomJokeByTerm(term){
 }
 
 function getRandomPage(totalPages) {
-    if(!totalPages) throw new SyntaxError('Incorrect total pages value')
+    if(!totalPages) 
+        throw new SyntaxError('Incorrect total pages value')
 
     if(totalPages === 1) return 1
 
@@ -67,27 +62,32 @@ function getRandomPage(totalPages) {
 }
 
 function getRandomNumberFromPage(metadata) {
-    if(!metadata) throw new SyntaxError('Incorrect metadata')
+    if(!metadata) 
+        throw new SyntaxError('Incorrect metadata')
 
-    if(metadata.total_pages === 1) return getRandomNumberByRange(metadata.total_jokes-1)
+    if(metadata.total_pages === 1) 
+        return getRandomNumberByRange(metadata.total_jokes-1)
 
     return getRandomNumberByRange(metadata.limit)
 }
 
 function printRandomJoke(randomJokeText){
-    if(!randomJokeText) throw new Error('Random joke is null')
+    if(!randomJokeText) 
+        throw new Error('Random joke is null')
 
     console.log(`Random joke: ${randomJokeText}`)
 }
 
 function printLeaderboard(mostPopularJokeText){
-    if(!mostPopularJokeText) throw new Error('The most popular joke is null')
+    if(!mostPopularJokeText) 
+        throw new Error('The most popular joke is null')
 
     console.log(`The most popular joke: ${mostPopularJokeText}`)
 }
 
 async function getJokesMetadataByTerm(term){
-    if(!term) throw new Error('Incorrect term!')
+    if(!term) 
+        throw new Error('Incorrect term!')
 
     const apiPath = `/search?term=${term}`
     const jokesMetadata = await getDataFromApi(apiPath)
@@ -96,7 +96,8 @@ async function getJokesMetadataByTerm(term){
 }
 
 async function getJokesFromPageByTerm(term, jokeApiPage){
-    if(!term) throw new Error('Incorrect term!')
+    if(!term) 
+        throw new Error('Incorrect term!')
 
     const apiPath = `/search?term=${term}&page=${jokeApiPage}`
     const jokes = (await getDataFromApi(apiPath)).results
@@ -105,7 +106,8 @@ async function getJokesFromPageByTerm(term, jokeApiPage){
 }
 
 function getDataFromApi(apiPath){
-    if(!apiPath) throw new Error('Incorrect path!')
+    if(!apiPath) 
+        throw new Error('Incorrect path!')
 
     const options = {
         hostname: process.env.HOSTNAME,
@@ -142,7 +144,8 @@ function getDataFromApi(apiPath){
 const getFormattedJoke = unformattedJoke => `${unformattedJoke.id}|${unformattedJoke.joke}\n`
 
 function getUnformattedJoke(formattedJoke){
-    if(!formattedJoke) throw new SyntaxError("Incorrect value")
+    if(!formattedJoke) 
+        throw new SyntaxError("Incorrect value")
 
     const jokeData = formattedJoke.split('|')
     const unformattedJoke = {
@@ -154,7 +157,8 @@ function getUnformattedJoke(formattedJoke){
 }
 
 function getUnformattedJokesArray(formattedJokes){
-    if(!formattedJokes) throw new SyntaxError("Incorrect jokes value")
+    if(!formattedJokes) 
+        throw new SyntaxError("Incorrect jokes value")
 
     let unformattedJokes = []
     formattedJokes
@@ -168,13 +172,17 @@ function getUnformattedJokesArray(formattedJokes){
 }
 
 function appendJokeToFile(joke, fileName, fileExtension){
-    if(!joke || !fileName, !fileExtension) throw new SyntaxError('Incorrect value')
+    if(!joke || !fileName, !fileExtension) 
+        throw new SyntaxError('Incorrect value')
 
     const formattedJoke = getFormattedJoke(joke)
     appendDataToFile(formattedJoke, fileName, fileExtension)
 }
 
 function appendDataToFile(data, fileName, fileExtension){
+    if(!data || !fileName, !fileExtension) 
+        throw new SyntaxError('Incorrect value')
+
     const filePath = `${fileName}.${fileExtension}`
 
     fs.appendFile(filePath, data, function (error) {
@@ -184,7 +192,8 @@ function appendDataToFile(data, fileName, fileExtension){
 }
 
 async function getJokesFromFile(fileName, fileExtension){
-    if(!fileName || !fileExtension) throw new SyntaxError('Incorrect file info')
+    if(!fileName || !fileExtension) 
+        throw new SyntaxError('Incorrect file info')
 
     const formattedJokes = await getDataFromFile(fileName, fileExtension)
     const unformattedJokes = getUnformattedJokesArray(formattedJokes)
@@ -193,7 +202,8 @@ async function getJokesFromFile(fileName, fileExtension){
 }
 
 function getDataFromFile(fileName, fileExtension){
-    if(!fileName || !fileExtension) throw new SyntaxError('Incorrect file info')
+    if(!fileName || !fileExtension) 
+        throw new SyntaxError('Incorrect file info')
 
     const result = new Promise((resolve, reject) => {
         const file = fs.createReadStream(`./${fileName}.${fileExtension}`);
@@ -223,7 +233,8 @@ function getMostPopularJoke(jokes){
 }
 
 function getMostPopularElement(array){
-    if(!array || !Array.isArray(array)) throw new SyntaxError('Incorrect value')
+    if(!array || !Array.isArray(array)) 
+        throw new SyntaxError('Incorrect value')
 
     const mostPopularElement = _.head(_(array)
         .countBy()
@@ -249,8 +260,12 @@ function getArgs () {
 }
 
 function main(){
-    const command = getArgs().longArgFlag
-    executeCommand(command)
+    try {
+        const command = getArgs().longArgFlag
+        executeCommand(command)
+    } catch (error) {
+        console.log(error.message)
+    }
 }
 
 main()
